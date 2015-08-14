d54925aa173e800450f5ef474892dd691d704af2
Zhen Li <zhenli336@gmail.com>
2015-08-14 12:36:34 +1200

    new file:   insertion sort.cpp

---
+++ Sorting/insertion sort.cpp
@@ -0,0 +1,32 @@
+﻿//complexity is O(n^2)
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int arr[] = { 5, 1, 6, 2, 4, 3 };
+	// length of array
+	int length = sizeof(arr) / sizeof(0);
+	int i, j, key;
+	//loop start from the second value
+	for (i = 1; i < length; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+		// loop start from the first value
+		while (j >= 0 && key < arr[j])
+		{
+			arr[j + 1] = arr[j];
+			j--;
+
+		}
+		arr[j + 1] = key;
+	}
+	for (int i = 0; i < length; ++i)
+		cout << arr[i];
+	system("pause");
+	return 0;
+}
