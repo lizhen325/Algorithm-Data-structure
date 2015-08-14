137a38efc8d8bdb2902856ece469baceb3cdd563
Zhen Li <zhenli336@gmail.com>
2015-08-14 15:13:06 +1200

    deleted:    insertion sort.cpp 	new file:   insertion_sort.cpp 	new file:   merge_sort.cpp

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+using namespace std;
+
+void merge_sort(int arr[], int start_index, int length)
+{
+	int j, k;
+	for (start_index; start_index < length; start_index++){
+		k = arr[start_index];
+		j = start_index - 1;
+		while (j >= 0 && k < arr[j]){
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = k;
+	}
+	for (int i = 0; i < length; ++i)
+		cout << arr[i];
+}
+
+int main()
+{
+	int arr[] = { 2, 3, 4, 1, 5 };
+	int length = sizeof(arr) / sizeof(0);
+	merge_sort(arr, 1, length);
+	system("pause");
+	return 0;
+}
---
+++ Sorting/merge_sort.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+void merge(int* input, int p, int r)
+{
+	// medium value
+	int mid = (p + r) / 2;
+	int i1 = 0;
+	//first of array. ex arr[] = {1,2,3} points 1.
+	int i2 = p;
+	//half of array ex arr[1,2,3,4,5,6] points 4
+	int i3 = mid + 1;
+
+	// new array with length of r called temp
+	int *temp = new int[r];
+
+	// Merge in sorted form the 2 arrays
+	while (i2 <= mid && i3 <= r){
+		if (input[i2] < input[i3])
+			temp[i1++] = input[i2++];
+		else
+			temp[i1++] = input[i3++];
+		}
+	// Merge the elements in left array
+	while (i2 <= mid)
+		temp[i1++] = input[i2++];
+
+	// Merge elements in right array
+	while (i3 <= r)
+		temp[i1++] = input[i3++];
+
+	// Move from temp array
+	for (int i = p; i <= r; i++)
+		input[i] = temp[i - p];
+}
+
+void merge_sort(int* input, int p, int r)
+{
+	if (p < r)
+	{
+		int mid = (p + r) / 2;
+		// first recursion merge elements left array
+		merge_sort(input, p, mid);
+		// second recursion merge elements right array
+		merge_sort(input, mid + 1, r);
+		// merge
+		merge(input, p, r);
+	}
+}
+
+int main()
+{
+	int a[] = { 500, 700, 800, 100, 300, 200, 900, 400, 1000, 600 };
+	cout << "Input: ";
+	int n = sizeof(a) / sizeof(0);
+	for (int i = 0; i < n; ++i)
+		cout << a[i] << " , ";
+	cout << endl;
+	merge_sort(a, 0, n);
+	cout << "Output: ";
+	for (int i = 0; i < n; ++i)
+		cout << a[i+1] << " , ";
+	cout << endl;
+	system("pause");
+	return 0;
+}
